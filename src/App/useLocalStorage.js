69a0dd6a5bc922625dde04889e684d85180adb6d
ItzelVa <itzelochoavallejo@gmail.com>
2024-03-08 09:57:19 -0600

    Implementation of useReducer in the custom hook useLocalStorage

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -2,11 +2,24 @@ import React from "react";
 
 export function useLocalStorage(itemName, initialValue) {
 
-    const [item,setItem] = React.useState(initialValue)
+  const [state,dispatch] = React.useReducer(reducer,initialState({initialValue}))
+  const {item,loading,error,} = state
 
-    const [loading, setLoading] = React.useState(true)
+  const onError = (error) => {
+    dispatch({type: actionTypes.error, payload: error})
+  }
 
-    const [error, setError] = React.useState(false)
+  const onLoading = () => {
+    dispatch({type: actionTypes.loading})
+  }
+
+  const onSave = (item) => {
+    dispatch({type: actionTypes.save, payload:item})
+  }
+
+    //const [item,setItem] = React.useState(initialValue)
+    //const [loading, setLoading] = React.useState(true)
+    //const [error, setError] = React.useState(false)
   
     React.useEffect( () => {  
       setTimeout( () => {
@@ -21,13 +34,17 @@ export function useLocalStorage(itemName, initialValue) {
         }
         else{
           parsedItem = JSON.parse(localStorageItem)
-          setItem(parsedItem)
+          //setItem(parsedItem)
+          onSave(parsedItem)
         }
   
-        setLoading(false)
+        //setLoading(false)
+        onLoading()
         } catch(error){
-          setLoading(false)
-          setError(true)
+          //setLoading(false)
+          //setError(true)
+          onLoading()
+          onError(error)
         }
       }, 3000)
       
@@ -36,11 +53,42 @@ export function useLocalStorage(itemName, initialValue) {
   
     const saveItem = (newItem) => {
       localStorage.setItem(itemName, JSON.stringify(newItem))
-      setItem(newItem)
+      //setItem(newItem)
+      onSave(newItem)
     }
 
     return {item,saveItem,loading,error}
   }
+
+const initialState = ({initialValue}) => ({
+  item : initialValue,
+  loading: true,
+  error: false,
+})
+
+const actionTypes = {
+  error: 'ERROR',
+  loading: 'LOADING',
+  save: 'SAVE'
+}
+
+const reducerObject = (state,payload) => ({
+  [actionTypes.error]: {
+    ...state,
+    error:true,
+  },
+  [actionTypes.loading]: {
+    ...state,
+    loading: false,
+  },
+  [actionTypes.save]: {
+    ...state,
+    item: payload
+  }
+})
+const reducer = (state,action) => reducerObject(state,action.payload)[action.type] || state
+
+
 
   // const defaultTodos = [
 //   {text: 'Cortar cebolla' , completed: false},
